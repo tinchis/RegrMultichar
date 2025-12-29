b35a1a84c98baf6867b3ab37f67fd604d0dd4c95
tinchis <marcuad05@gmail.com>
2025-12-29 17:38:35 -0300

    Revert "a"
    This reverts commit 3e9d803a2290061a7ced28b1dff8ee72edf55556.

--- ui/src/components/registeration/Register.jsx
+++ ui/src/components/registeration/Register.jsx
@@ -137,17 +137,17 @@ const Register = () => {
   useEffect(() => {
     if (shouldShow) {
       document.documentElement.classList.add('dark')
-      return () => {
-        document.documentElement.classList.remove('dark')
-      }
     } else {
+      document.documentElement.classList.remove('dark')
+    }
+    return () => {
       document.documentElement.classList.remove('dark')
     }
   }, [shouldShow])
 
   return (
     <div
-      className='flex flex-col items-end justify-center h-screen an'
+      className='flex flex-col items-end justify-center h-screen an dark'
       style={{ display: shouldShow ? 'flex' : 'none' }}
       ref={ref}
       // onKeyDown={handlekey}
